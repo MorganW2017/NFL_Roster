763228aabefbdf893dee19f42019689a842dffec
Morgan Waters <morangwaters@gmail.com>
2017-11-06 17:03:59 -0700

    Data is loading, but not showing. Almost have the eyes

--- app/roster/player-controller.js
+++ app/roster/player-controller.js
@@ -1,16 +1,16 @@
-function PlayerService() {
-    var playerService = new PlayerService();
+function PlayersController() {
+    var playerService = new PlayerService(drawPlayerList);
     var playerElem = document.getElementById('player-list')
     var showButton = document.getElementById('show-button')
     this.add = function add(id) {
         console.log(playerId, id)
     }
-    playerService.getPlayers(ready)
+    // playerService.getPlayers(ready)
     function ready(data) {
         updatePlayer(data)
         function updatePlayer(list) {
             var elem = document.getElementById('players')
-            function drawplayerList() {
+            function drawPlayerList(list) {
                 elem.innerHTML = ''
                 var playerTemplate = ''
                 for (var i in list) {
@@ -18,7 +18,7 @@ function PlayerService() {
                     player.thumbnail.path = player.thumbnail.path.replace('http:', '')
                     marvelTemplate += `
                     <div class="card">
-                    <img src="${player.thumbnail.path}.${character.thumbnail.extension}" wdith="100">
+                    <img src="${player.thumbnail.path}.${player.thumbnail.extension}" wdith="100">
                     <h3>${player.name}{/h3}
                     <div>
                     <button class="btn-success" id="${player.id}" onclick="app.roster.player-controller.add('${player.id}')">Add to Team</button>
--- app/roster/player-service.js
+++ app/roster/player-service.js
@@ -1,38 +1,44 @@
-PlayerService = function () {
+function PlayerService(callback) {
     var players = [];
     var myPlayers = [];
-    var PlayersService = function (callback) {
-        var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
-        var playersData = [];
-        this.getPlayersByTeam = function (teamName) {
-            // ...
-        }
-        this.getPlayersByPosition = function (position) {
-            // ...
-        }
-        function loadPlayersData() {
-            //Lets check the localstorage for the data before making the call.
-            //Ideally if a user has already used your site 
-            //we can cut down on the load time by saving and pulling from localstorage 
-            var localData = localStorage.getItem('playersData');
-            if (localData) {
-                playersData = JSON.parse(localData);
-                return callback();
-                //return will short-circuit the loadPlayersData function
-                //this will prevent the code below from ever executing
-            }
-            var url = "https://bcw-getter.herokuapp.com/?url=";
-            var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
-            var apiUrl = url + encodeURIComponent(endpointUri);
-            $.getJSON(apiUrl, function (data) {
-                playersData = data.body.players;
-                console.log('Player Data Ready')
-                console.log('Writing Player Data to localStorage')
-                localStorage.setItem('playersData', JSON.stringify(playersData))
-                console.log('Finished Writing Player Data to localStorage')
-                callback()
-            });
-        }
-        loadPlayersData(); //call the function above every time we create a new service
+    var playersData = [];
+    this.getPlayersByTeam = function (teamName) {
+        // ...
     }
+    this.getPlayersByPosition = function (position) {
+        // ...
+    }
+    function loadPlayersData() {
+        //Lets check the localstorage for the data before making the call.
+        //Ideally if a user has already used your site 
+        //we can cut down on the load time by saving and pulling from localstorage 
+        var localData = localStorage.getItem('playersData');
+        if (localData) {
+            playersData = JSON.parse(localData);
+            console.log(playersData)
+            return callback(playersData);
+            //return will short-circuit the loadPlayersData function
+            //this will prevent the code below from ever executing
+        }
+        var baseurl = "https://bcw-getter.herokuapp.com/?url=";
+        var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+        var apiUrl = baseurl + encodeURIComponent(endpointUri);
+        $.getJSON(apiUrl, function (data) {
+            playersData = data.body.players;
+            console.log('Player Data Ready')
+            console.log('Writing Player Data to localStorage')
+            localStorage.setItem('playersData', JSON.stringify(playersData))
+            console.log('Finished Writing Player Data to localStorage')
+            callback(playersData)
+        });
+    }
+    loadPlayersData(); //call the function above every time we create a new service
 }
+    // this.getPlayers = function(callWhenDone){
+    //     //Use &offset=Number to add pagination
+    //     $.get(baseUrl + 'players' + endpointUri, function(response){
+    //       players = response.data.results;
+    //       callWhenDone(players)
+    //     })
+    //   }
+
